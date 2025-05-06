b5f406da79a9408568bbe772471c11f7be2a19a4
LNC COMMUNITY <108217474+LNC07@users.noreply.github.com>
2025-05-06 14:58:05 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyB3-ClSSDlKoVANTJOAYcnB3SZpEXjIBDE",
+  authDomain: "fast-earn-5a485.firebaseapp.com",
+  projectId: "fast-earn-5a485",
+  storageBucket: "fast-earn-5a485.firebasestorage.app",
+  messagingSenderId: "897353554923",
+  appId: "1:897353554923:web:7010437d6353afa3ae17ae",
+  measurementId: "G-VD8Q9CE9NX"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
